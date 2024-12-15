373744d1ef20d9c0520b28312407c3c4acb5cfe5
ashagre <ashagrenegash987@gmail.com>
2024-12-15 20:35:10 +0200

    add new project

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,44 +1,25 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import NavBar from './components/NavBar';
-import Login from './pages/Login';
-import Home from './pages/Home';
-import ServerCheck from './pages/ServerCheck'; // הוספת הקומפוננטה ServerCheck
+import logo from './logo.svg';
+import './App.css';
 
-const App = () => {
-    const [token, setToken] = React.useState(null);
-
-    const handleLogin = (userToken) => {
-        // שים את ה-token ב-localStorage כדי לשמור אותו גם אחרי רענון
-        localStorage.setItem('userToken', userToken);
-        setToken(userToken);
-    };
-
-    React.useEffect(() => {
-        // אם יש Token ב-localStorage, תעדכן את ה-state עם הערך
-        const savedToken = localStorage.getItem('userToken');
-        if (savedToken) {
-            setToken(savedToken);
-        }
-    }, []); // זה יקרה פעם אחת בעת טעינת ה-Component
-
-    return (
-        <Router>
-            <div>
-                {token && <NavBar />}
-                <Routes>
-                    {/* אם אין token, מציג את דף ההתחברות */}
-                    {!token ? (
-                        <Route path="/" element={<Login onLogin={handleLogin} />} />
-                    ) : (
-                        <Route path="/" element={<Home />} />
-                    )}
-                    {/* נתיב לבדוק אם השרת פועל */}
-                    <Route path="/check-server" element={<ServerCheck />} />
-                </Routes>
-            </div>
-        </Router>
-    );
-};
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
 
 export default App;
