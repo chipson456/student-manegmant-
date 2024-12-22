b8c5249e3f95a9e771ee7f67657da869efbd8a3e
nadavz1981 <nadavz1981@gmail.com>
2024-12-22 18:27:16 +0200

    a

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Login from "./copms/Login";
+import NavBar from "./copms/Navbar";
+
 
 function App() {
   return (
     <div>
+      <NavBar />
       <Login />
       <h2>נדב</h2>
     </div>
