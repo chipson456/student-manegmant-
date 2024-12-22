e56c754b3797012a4468ee6b686111dc5fcaaaf1
Ben <benleviae5@gmail.com>
2024-12-22 19:40:48 +0200

    login upgrade

--- frontend/src/copms/Login.jsx
+++ frontend/src/copms/Login.jsx
@@ -1,6 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
+  const [userName, setUserName] = useState("");
+  const [password, SetPassword] = useState("");
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    console.log(`userName : ${userName},
+      Password : ${password}`);
+
+    /*  try {
+      const response = await axios.post("http://localhost:5000/api/login", {
+        userName,
+        password,
+      });
+      alert(response.data.message); // הודעה במקרה של התחברות
+    } catch (error) {
+      if (error.response && error.response.data) {
+        alert(error.response.data.message);
+      } else {
+        alert("שגיאה בחיבור לשרת");
+      }
+    }
+ */
+  };
+
   return (
     <div className="d-flex justify-content-center align-items-center vh-100">
       <div
@@ -8,23 +32,27 @@ const Login = () => {
         style={{ width: "300px", borderRadius: "10px" }}
       >
         <h2 className="text-center mb-4">התחברות</h2>
-        <div className="mb-3">
-          <input
-            type="text"
-            className="form-control"
-            placeholder="שם משתמש :"
-          />
-        </div>
-        <div className="mb-3">
-          <input
-            type="password"
-            className="form-control"
-            placeholder="סיסמה :"
-          />
-        </div>
-        <button type="button" className="btn btn-dark w-100">
-          התחבר
-        </button>
+        <form onSubmit={handleLogin}>
+          <div className="mb-3">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="שם משתמש :"
+              onChange={(e) => setUserName(e.target.value)}
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              type="password"
+              className="form-control"
+              placeholder="סיסמה :"
+              onChange={(e) => SetPassword(e.target.value)}
+            />
+          </div>{" "}
+          <button type="submit" className="btn btn-dark w-100">
+            התחבר
+          </button>{" "}
+        </form>
       </div>
     </div>
   );
