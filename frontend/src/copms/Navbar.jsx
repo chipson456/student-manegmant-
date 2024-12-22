042ffb798e19fbe022dc844e95f995ef6c543b88
Ben <benleviae5@gmail.com>
2024-12-22 18:53:36 +0200

    first commit

--- frontend/src/copms/Navbar.jsx
+++ frontend/src/copms/Navbar.jsx
@@ -1,22 +1,22 @@
-import React from 'react';
-import { Navbar, Container, Nav } from 'react-bootstrap';
+import React from "react";
+import { Navbar, Container, Nav } from "react-bootstrap";
 
 const NavBar = () => {
-    return (
-        <Navbar bg="dark" variant="dark" expand="lg">
-            <Container>
-                <Navbar.Brand href="#">אתר המכללה</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link href="#">בית</Nav.Link>
-                        <Nav.Link href="#">עלינו</Nav.Link>
-                        <Nav.Link href="#">יצירת קשר</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-    );
+  return (
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <Container>
+        <Navbar.Brand href="#">אתר המכללה</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#">בית</Nav.Link>
+            <Nav.Link href="#">עלינו</Nav.Link>
+            <Nav.Link href="#">יצddירת קשר</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default NavBar;
